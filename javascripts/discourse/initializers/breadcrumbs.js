9b54f45cdb4ec9d46a8d2a12feecfd4bf3809cde
denvergeeks <jen@denvergeeks.com>
2026-07-27 23:20:13 -0600

    Fix homeIcon assignment to use string literal

--- javascripts/discourse/initializers/breadcrumbs.js
+++ javascripts/discourse/initializers/breadcrumbs.js
@@ -3,7 +3,7 @@ import BreadCrumbs from "../components/breadcrumbs";
 import config from "../lib/breadcrumb-config";
 
 export default apiInitializer("1.14.0", (api) => {
-  config.homeIcon = settings.home_icon || house;
+  config.homeIcon = settings.home_icon || "house";
   config.homeLabel = settings.home_label || "Home";
   config.homeUrl = settings.home_url || "/";
 
